c451e98e43484d629a1183f63298dfd7e75b7d84
fridasesangari <sesangari.frida@gmail.com>
2026-05-19 01:07:54 -0600

    ruta incidentes

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,9 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: "aws-1-us-west-1.pooler.supabase.com",
-    user: "postgres.txrwtweduzcakwwklbgd",
-    password: "alertavioleta.",
-    database: "postgres",
-    port: 6543,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
-});
-
-pool.connect((err) => {
-    if (err) {
-        console.log("Error de conexión:", err);
-    } else {
-        console.log("PostgreSQL conectado");
     }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const pool = require("./db");
+
+dotenv.config();
 
 const app = express();
 
@@ -10,7 +14,61 @@ app.get("/", (req, res) => {
     res.send("Backend funcionando 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+
+// OBTENER INCIDENTES
+app.get("/incidentes", async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT * FROM incidentes ORDER BY id DESC"
+        );
+
+        res.json(result.rows);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "Error obteniendo incidentes"
+        });
+    }
+});
+
+
+// CREAR INCIDENTE
+app.post("/incidentes", async (req, res) => {
+
+    try {
+
+        const {
+            titulo,
+            descripcion,
+            latitud,
+            longitud
+        } = req.body;
+
+        const result = await pool.query(
+            `
+            INSERT INTO incidentes
+            (titulo, descripcion, latitud, longitud)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *
+            `,
+            [titulo, descripcion, latitud, longitud]
+        );
+
+        res.json(result.rows[0]);
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            error: "Error creando incidente"
+        });
+    }
+});
+
+
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
